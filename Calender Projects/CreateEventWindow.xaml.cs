6ac39c2dda299f2fbcd39184249a956d29fa1605
Lawrence Douglas <lawrencelegend123@gmail.com>
2017-08-10 13:57:39 -0600

    First Commit

---
+++ Calender Projects/CreateEventWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calender_Projects
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveEvent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GoBackEvent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
